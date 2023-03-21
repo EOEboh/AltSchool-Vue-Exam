0f0a9c23699df77f495abf9da372447022dd8fc2
EOEboh <ecolejnr007@gmail.com>
2023-03-21 08:10:15 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+<template>
+   <router-view v-slot="{ Component }" > 
+<transition name="route" mode="out-in" appear>
+<component :is="Component" />
+</transition>
+  </router-view>
+</template>
+
+<style scoped>
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(100px)
+}
+.route-enter-active,
+.route-leave-active {
+  transition: all .2s ease-out
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(100px)
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+  transpileDependencies: true,
+configureWebpack: {
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000
+      }
+    }
+  }
+})
